beafe5e2faad5de43f36a444c3644379f4edab7c
troingle <bugsinsidemyhead@gmail.com>
2025-08-11 19:11:44 +0300

    Added line of sight for enemies

--- characters/enemy/enemy.gd
+++ characters/enemy/enemy.gd
@@ -7,52 +7,56 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @onready var nav = $NavigationAgent3D
 
-@onready var rc = $NPCHead/RayCast3D
+@onready var player_detector = $EnemyHead/PlayerDetector
 
 @onready var wait_timer = $WaitTimer
 @onready var failsafe_timer = $FailsafeTimer
 
 @onready var player = $"../../Player"
 
-var detected = true
+var detected = false
 
 var rng = RandomNumberGenerator.new()
 
 var hp = 1
 
+var rotate_speed = 140.0
+
 func _ready() -> void:
 	pass
 	
 func _physics_process(delta: float) -> void:
+	var dir = Vector3()
+	#$BodyMesh/AnimationPlayer.play("RunForward")
+
+	nav.target_position = player.global_position
+	
 	if detected:
-		var dir = Vector3()
-		#$BodyMesh/AnimationPlayer.play("RunForward")
-
-		nav.target_position = player.global_position
-
 		dir = nav.get_next_path_position() - global_position
 		dir.y = 0
 		dir = dir.normalized()
 
 		velocity.x = lerp(velocity.x, dir.x * speed, accel * delta)
 		velocity.z = lerp(velocity.z, dir.z * speed, accel * delta)
-
-		if not is_on_floor():
-			velocity.y -= gravity * delta
-		else:
-			velocity.y = 0
-		
-		$BodyMesh.look_at(global_position + dir, Vector3.UP)
-		$BodyMesh.rotation.x = 0
-		
-		move_and_slide()
-
 	else:
-		pass#$BodyMesh/AnimationPlayer.play("CombatIdle")
+		$EnemyHead.rotation_degrees.y += rotate_speed * delta
+		if player_detector.is_colliding():
+			if player_detector.get_collider().is_in_group("player"):
+				detected = true
+			
+
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	else:
+		velocity.y = 0
 	
+	$BodyMesh.look_at(global_position + dir, Vector3.UP)
+	$BodyMesh.rotation.x = 0
 	
 	if hp < 0:
 		die()
-
+		
+	move_and_slide()
+	
 func die():
 	queue_free()
