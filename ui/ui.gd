d9199438218e3b73286ed950dddb7a9217655f5d
troingle <bugsinsidemyhead@gmail.com>
2025-08-16 16:41:02 +0300

    Modified UI a bit

--- ui/ui.gd
+++ ui/ui.gd
@@ -1,7 +1,9 @@
 extends Control
 
 @onready var hp_label = $HPContainer/HPLabel
+@onready var ammo_label = $AmmoContainer/AmmoLabel
 @onready var player = $".."
 
 func _process(delta: float) -> void:
-	hp_label.text = "HEALTH: " + str(Global.hp) + "\nAMMO: " + str(Global.ammo) + "\nTOTAL AMMO: " + str(Global.reserve_ammo)
+	hp_label.text = str(Global.hp)
+	ammo_label.text = str(Global.ammo) + " / " + str(Global.reserve_ammo)
